2c85b6fd990a6cd15aca728ef95f04f8b372ec28
mugmod <romadenisov264@gmail.com>
2025-10-20 20:31:53 +0500

    сделал тесты не через console.log, а просто через вызов функций

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@
  * Функция для проверки длины строки
  * @param {string} string - Проверяемая строка
  * @param {number} maxLength - Максимальная допустимая длина
- * @returns {boolean} - выведет true, если строка меньше или равна, иначе выведет false
+ * @returns {boolean} - true, если строка меньше или равна максимальной длине, иначе false
  */
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
@@ -10,20 +10,31 @@ const checkStringLength = (string, maxLength) => string.length <= maxLength;
 /**
  * Функция для проверки, является ли строка палиндромом
  * @param {string} string - Проверяемая строка
- * @returns {boolean} - выведет true, если строка является палиндромом, иначе выведет false
+ * @returns {boolean} - true, если строка является палиндромом, иначе false
  */
 const isPalindrome = (string) => {
-  // Убирает пробелы и переводит строку в нижний регистр
+  // Нормализуем строку: убираем пробелы и приводим к нижнему регистру
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
 
-  // Перевёрнутая строка
+  // Создаем перевернутую строку
   let reversedString = '';
 
-  // Цикл для создания reversedString
+  // Цикл для создания перевернутой строки
   for (let i = normalizedString.length - 1; i >= 0; i--) {
     reversedString += normalizedString[i];
   }
 
-  // Сравниваем нормальную строку и перевёрнутую
+  // Сравниваем нормализованную строку с перевернутой
   return normalizedString === reversedString;
 };
+
+// Тест функции checkStringLength
+checkStringLength('проверяемая строка', 20); // true
+checkStringLength('проверяемая строка', 18); // true
+checkStringLength('проверяемая строка', 10); // false
+
+// Тест функции isPalindrome
+isPalindrome('топот'); // true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс'); // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
