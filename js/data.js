0320b94b71a124a0c5b9d9bf5775c40a1eeeaa1e
mugmod <romadenisov264@gmail.com>
2025-12-09 01:10:08 +0500

    Сделал задание 7.11

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
-// Исходные данные
+import { getRandomInteger, createRandomIdFromRangeGenerator } from './util.js';
 
+// Исходные данные
 export const userNames = [
   "Александр", "Мария", "Дмитрий", "Анна", "Сергей",
   "Екатерина", "Андрей", "Ольга", "Алексей", "Наталья",
@@ -28,3 +29,39 @@ export const photoDescriptions = [
   "Рыбалка на рассвете", "Дождь за окном", "Велосипедная прогулка",
   "Фестиваль уличной еды", "Зимние забавы", "Заброшенное здание"
 ];
+
+// Генерация комментариев
+const generateIdComment = createRandomIdFromRangeGenerator(1, 750);
+
+const createComment = (quantity) => {
+  const comments = [];
+  for (let i = 0; i < quantity; i++) {
+    comments.push({
+      id: generateIdComment(),
+      avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+      message: userComments[getRandomInteger(0, userComments.length - 1)],
+      name: userNames[getRandomInteger(0, userNames.length - 1)]
+    });
+  }
+  return comments;
+};
+
+// Генерация одного поста
+const createSinglePost = (generateId, generateUrl) => ({
+  id: generateId(),
+  url: `photos/${generateUrl()}.jpg`,
+  description: photoDescriptions[getRandomInteger(0, photoDescriptions.length - 1)],
+  likes: getRandomInteger(15, 200),
+  comments: createComment(getRandomInteger(0, 30))
+});
+
+// Генерация массива постов
+export const createPosts = (quantity) => {
+  const generateId = createRandomIdFromRangeGenerator(1, quantity);
+  const generateUrl = createRandomIdFromRangeGenerator(1, quantity);
+  const posts = [];
+  for (let i = 0; i < quantity; i++) {
+    posts.push(createSinglePost(generateId, generateUrl));
+  }
+  return posts;
+};
